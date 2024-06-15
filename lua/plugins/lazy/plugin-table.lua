5cf4f1ca0bb7d009897f1d5596276e3247fe0756
bluberry-dream <khan.ssjc@gmail.com>
2024-06-15 15:43:23 -0400

    feat: nvim-cmp and conform plugins

--- lua/plugins/lazy/plugin-table.lua
+++ lua/plugins/lazy/plugin-table.lua
@@ -62,7 +62,61 @@ local M = {
 		event = "VeryLazy", -- Sets the loading event to 'VeryLazy'
 		config = require("plugins.which-key.config"),
 	},
-	-- Fuzzy Finder (files, lsp, etc)
+	-- LSP Configuration & Plugins
+  {
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			-- Automatically install LSPs and related tools to stdpath for neovim
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+
+			-- Useful status updates for LSP.
+			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+			{ "j-hui/fidget.nvim", opts = {} },
+		},
+		config = require("plugins.nvim-lspconfig.config"),
+	},
+ 	 -- Autocompletion
+  {
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			-- Snippet Engine & its associated nvim-cmp source
+			{
+				"L3MON4D3/LuaSnip",
+				build = (function()
+					-- Build Step is needed for regex support in snippets
+					-- This step is not supported in many windows environments
+					-- Remove the below condition to re-enable on windows
+					if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+						return
+					end
+					return "make install_jsregexp"
+				end)(),
+			},
+			"saadparwaiz1/cmp_luasnip",
+
+			-- Adds other completion capabilities.
+			--  nvim-cmp does not ship with all sources by default. They are split
+			--  into multiple repos for maintenance purposes.
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-path",
+
+			-- If you want to add a bunch of pre-configured snippets,
+			--    you can use this plugin to help you. It even has snippets
+			--    for various frameworks/libraries/etc. but you will have to
+			--    set up the ones that are useful for you.
+			-- 'rafamadriz/friendly-snippets',
+		},
+	},
+  -- Autoformatter
+  -- {
+  --     "stevearc/conform.nvim",
+  --     opts = require("plugins.conform.opts")
+  -- }
+
+
 }
 
 return M
